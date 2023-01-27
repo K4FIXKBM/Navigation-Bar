1bf93f150b52103df380e51606838f84a0397341
K4FIXKBM <kafisarder@gmail.com>
2023-01-27 20:18:10 +0600

    React Navigatio bar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './Main';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './Navbar';
+import { useState } from 'react';
+function App() {
+  
+  return (
+    <div className="App">
+        <Navbar/>
+    </div>
+  );
+}
+
+export default App;
